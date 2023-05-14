5eabc4a5812a149ec0e1cdf187d45ffaa47414fe
Ashique <ashique12009@gmail.com>
2023-05-14 14:52:33 +0600

    header and footer layout set, css added, read product done first phase

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>.:Welcome to PHP CRUD in OOP way:.</title>
-</head>
-<body>
+<?php
+    require_once "includes/config/class-database.php";
+    require_once "includes/config/class-create-table.php";
+    $classDatabase                  = new ClassDatabase();
+    $classDatabaseConnectionObject  = $classDatabase->getConnection();
+    $createTable                    = new ClassCreateTable($classDatabaseConnectionObject);
 
-    <?php
-        require_once "includes/config/class-database.php";
-        require_once "includes/config/class-create-table.php";
-        $classDatabase                  = new ClassDatabase();
-        $classDatabaseConnectionObject  = $classDatabase->getConnection();
-        $createTable                    = new ClassCreateTable($classDatabaseConnectionObject);
+    $createTable->createDatabaseTables();
+?>
 
-        $createTable->createDatabaseTables();
-    ?>
-
-</body>
-</html>
+<?php
+// set page headers
+$page_title = "Read Product";
+include_once "layout_header.php";
+ 
+// contents will be here
+echo "<div class='right-button-margin'><a href='create_product.php' class='btn btn-default pull-right'>Create Product</a></div>";
+ 
+// footer
+include_once "layout_footer.php";
+?>
